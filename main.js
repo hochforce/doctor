f4134a296bb560e4613c27f2742c14ff1bc8771e
Hugo Hoch <hochforce@gmail.com>
2022-09-21 15:48:45 -0300

    Mobile first

---
+++ main.js
@@ -0,0 +1,31 @@
+function onScroll() {
+  let nav = document.getElementById('navigation')
+  if (scrollY > 0) {
+   
+    nav.classList.add('scroll')
+  } else {
+    nav.classList.remove('scroll')
+  }
+}
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img, 
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content
+`);
